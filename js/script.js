8d3c29067e7ea9ac12a24e61c5405554026fc053
Helder-Machava <mac.ilearning@gmail.com>
2022-01-28 18:20:30 +0200

    slide add from swiper

--- js/script.js
+++ js/script.js
@@ -36,3 +36,49 @@ function btn2() {
     }
 
 }
+
+//SWIPER SLIDE
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+  
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
+
+  //LOADER
+  var loader = document.querySelector('#preloader')
+  window.addEventListener("load", function () {
+      loader.style.display = "none  "
+  })

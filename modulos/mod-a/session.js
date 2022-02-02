8442065ad48fe9e4cef72f5aa9df8db252388268
Helder-Machava <81919768+Helder-Machava@users.noreply.github.com>
2022-02-02 22:43:11 +0200

    final ajust

--- modulos/mod-a/session.js
+++ modulos/mod-a/session.js
@@ -1,11 +1,11 @@
 
 //CONTENT TO BE FILTER (Cntt)
 var Ex1 = document.querySelector('.ex1')
-var BtnEx1 = document.querySelector('.btn-ex1')
-
 var Ex2 = document.querySelector('.ex2')
 var Ex3 = document.querySelector('.ex3')
 var Ex4 = document.querySelector('.ex4')
+
+var BtnEx1 = document.querySelector('.btn-ex1')
 
 var noContent = document.querySelector('.off') 
 
@@ -14,15 +14,15 @@ var noContent = document.querySelector('.off')
 
 function btn1() {
     if (Ex1.style.display === 'none'|| BtnEx1.style.display === 'none'|| noContent.style.display === 'block') {
-        Ex1.style.display = 'block'
-        BtnEx1.style.display = 'block'
+        Ex1.style.display = 'flex'
+        BtnEx1.style.display = 'flex'
 
         noContent.style.display = 'none'
 
     }
      else {
-        Ex1.style.display = 'block'
-        BtnEx1.style.display = 'block'
+        Ex1.style.display = 'flex'
+        BtnEx1.style.display = 'flex'
         noContent.style.display = 'none'
 
     }
@@ -30,7 +30,39 @@ function btn1() {
 }
 
 function btn2() {
-    if (Ex1.style.display === 'block' || BtnEx1.style.display === 'block'|| noContent.style.display === 'none') {
+    if (Ex1.style.display === 'flex' || BtnEx1.style.display === 'flex'|| noContent.style.display === 'none') {
+        Ex1.style.display = 'none'
+        BtnEx1.style.display = 'none'
+
+        noContent.style.display = 'block'
+    }
+     else {
+        Ex1.style.display = 'none'
+        BtnEx1.style.display = 'none'
+
+        noContent.style.display = 'block'
+
+    }
+
+}
+function btn3() {
+    if (Ex1.style.display === 'flex' || BtnEx1.style.display === 'flex'|| noContent.style.display === 'none') {
+        Ex1.style.display = 'none'
+        BtnEx1.style.display = 'none'
+
+        noContent.style.display = 'block'
+    }
+     else {
+        Ex1.style.display = 'none'
+        BtnEx1.style.display = 'none'
+
+        noContent.style.display = 'block'
+
+    }
+
+}
+function btn4() {
+    if (Ex1.style.display === 'flex' || BtnEx1.style.display === 'flex'|| noContent.style.display === 'none') {
         Ex1.style.display = 'none'
         BtnEx1.style.display = 'none'
 

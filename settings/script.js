a937f0191bd1f9dee9756a040da67c6e74c55656
Helder-Machava <81919768+Helder-Machava@users.noreply.github.com>
2022-02-05 21:11:34 +0200

    Update script.js

--- settings/script.js
+++ settings/script.js
@@ -23,9 +23,9 @@ window.onload = ()=>{
                 closeIcon.onclick = ()=>{ //hide toast notification on close icon click
                     wrapper.classList.add("hide");
                 }
-                setTimeout(()=>{ //hide the toast notification automatically after 5 seconds
+                setTimeout(()=>{ //hide the toast notification automatically after 3 seconds
                     wrapper.classList.add("hide");
-                }, 2500);
+                }, 3000);
             }else{
                 offline(); //calling offline function if ajax status is not equal to 200 or not less that 300
             }

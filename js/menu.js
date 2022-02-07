65a53255db3cd60d391bb3e885160ba0a84c3979
Helder-Machava <81919768+Helder-Machava@users.noreply.github.com>
2022-02-07 20:47:56 +0200

    great changes

--- js/menu.js
+++ js/menu.js
@@ -2,22 +2,39 @@ window.addEventListener("load", ()=>{
     document.body.classList.remove("preload")
 })
 
+//Increment code for blur added
+let blure = document.querySelector('.overlay_first')
+//Increment code for blur added
+
+
 document.addEventListener("DOMContentLoaded", () =>{
     const nav = document.querySelector('.nav')
 
     document.querySelector('#btnNav').addEventListener("click", ()=>{
         nav.classList.add("nav--open")
+        blure.style.display='block'
+
     })
 
     document.querySelector('.nav__overlay').addEventListener("click", ()=>{
         nav.classList.remove("nav--open")
+            blure.style.display='none'
+
+    })
+    document.querySelector('#homePage').addEventListener("click", ()=>{
+        nav.classList.remove("nav--open")
+            blure.style.display='none'
+
     })
 
     document.querySelector('.voltar').addEventListener("click", ()=>{
         nav.classList.remove("nav--open")
+            blure.style.display='none'
+
     })
 
 })
+
 
 
 //This code used to rotate drop icon(-180deg)
